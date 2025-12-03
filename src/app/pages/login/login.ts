617bb6f11921096d7d063a10e98e1157abba22a3
Golu <shvansh2903@gmail.com>
2025-12-03 14:43:44 +0530

    login page new

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+  standalone: true,
+})
+export class Login {}

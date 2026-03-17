dcc2128c35ec0db7c60440bcc2671e9c9d5394c6
Vansh Sharma <shvansh2903@gmail.com>
2026-03-17 14:48:09 +0530

    clean android

--- android/app/src/main/java/com/roadradar/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.roadradar.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.roadradar.app',
-  appName: 'RoadRadar',
-  webDir: 'www'
-};
-
-export default config;

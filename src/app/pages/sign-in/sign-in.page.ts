777a533840ce6f794d453cba78a0bfae7ae9b3e8
Vansh Sharma <shvansh2903@gmail.com>
2026-03-18 16:31:06 +0530

    auth guard

--- src/app/pages/sign-in/sign-in.page.ts
+++ src/app/pages/sign-in/sign-in.page.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Route, Router } from '@angular/router';
-import { IonHeader, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  NavController,
+} from '@ionic/angular/standalone';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { NavigationService } from 'src/app/services/navigation';
@@ -9,34 +14,55 @@ import { NavigationService } from 'src/app/services/navigation';
   templateUrl: './sign-in.page.html',
   styleUrls: ['./sign-in.page.scss'],
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
 })
 export class SignInPage implements OnInit {
   email: string = '';
   password: string = '';
   constructor(
-    public navService:NavigationService,
+    public navService: NavigationService,
+    private navCtrl: NavController,
     private router: Router,
     private authservice: AuthService,
   ) {}
 
   ngOnInit() {}
   goToForgot() {
-    this.router.navigate(['forgot-password']);
+    this.navCtrl.navigateRoot(['forgot-password']);
   }
   goToSignUp() {
-    this.router.navigate(['/register']);
+    this.navCtrl.navigateRoot(['/register']);
   }
+  loading = false;
 
   async login() {
+    if (this.loading) {
+      return;
+    }
+    if (!this.email || !this.password) {
+      alert('Please Enter Email and Password');
+      return;
+    }
+    if (this.password.length < 6) {
+      alert('Password must be at least 6 character');
+      return;
+    }
+    this.loading = true;
     try {
       await this.authservice.login(this.email, this.password);
-      this.router.navigate(['/dashboard']);
     } catch (err: any) {
       alert(err.message);
+    } finally {
+      this.loading = false;
     }
   }
   goToDashboard() {
     this.router.navigate(['/dashboard']);
+  }
+
+  showPassword = false ;
+
+  togglePassword(){
+    this.showPassword=!this.showPassword;
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,29 +1,39 @@
 import { Injectable } from '@angular/core';
 import { supabase } from '../supabase';
-
+import { NavController } from '@ionic/angular';
+import { NavigationService } from './navigation';
+import { NavigationCancel } from '@angular/router';
+import { SupabaseClient } from '@supabase/supabase-js';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  constructor(private navCtrl: NavController) {}
   async register(email: string, password: string) {
-    const {data,error}=await supabase.auth.signUp({
-      email,password
-    });
-    if (error) throw error ;
-    return data;
-  }
-  async login(email:string , password:string){
-    const {data,error}=await supabase.auth.signInWithPassword({
-      email,password
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
     });
     if (error) throw error;
-    return data 
+    return data;
   }
-  async logout (){
+  async login(email: string, password: string) {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error) {
+      throw error;
+      return data;
+    }
+
+    this.navCtrl.navigateRoot('/dashboard');
+  }
+  async logout() {
     await supabase.auth.signOut();
   }
-  async getUser (){
-    const {data} = await supabase.auth.getUser();
+  async getUser() {
+    const { data } = await supabase.auth.getUser();
     return data.user;
   }
 }

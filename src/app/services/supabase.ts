1dd90ebf7e600ae22142041fa0c6cd43757ea2de
Vansh Sharma <shvansh2903@gmail.com>
2026-05-14 15:07:11 +0530

    anon key

--- src/app/services/supabase.ts
+++ src/app/services/supabase.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://txfyrephofxrqhodbswe.supabase.co';
-const supabaseKey = 'YOUR_SUPABASE_KEY';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4ZnlyZXBob2Z4cnFob2Ric3dlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0NjIwMzEsImV4cCI6MjA4ODAzODAzMX0.mfB9B7OHsGqJSgRSMdEn_amOO10PO2phr5qZK84EvSQ'
 
 @Injectable({
   providedIn: 'root',
